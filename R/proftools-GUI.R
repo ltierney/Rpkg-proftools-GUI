2c395d4472052b3a84e47e41f6df375c868b5805
Riad-Jarjour <riadgold@gmail.com>
2014-03-23 13:59:32 -0500

    Added toolkit argument

--- R/proftools-GUI.R
+++ R/proftools-GUI.R
@@ -134,7 +134,8 @@ offspringFunSum <- function(path, self.gc) {
 
 startWidget <- function(pd = NULL, value = c("pct", "time", "hits"),
                         self = FALSE, srclines = TRUE, gc = TRUE,
-                        maxdepth = 10, treeType="funSum"){
+                        maxdepth = 10, treeType="funSum", toolkit="RGtk2"){
+    options(guiToolkit = toolkit)
     win <- gwindow("Hot Path Tree", height=700, width=1000)
     ## Remove widgetMenu from previous session
     if(exists("widgetMenu")) 
