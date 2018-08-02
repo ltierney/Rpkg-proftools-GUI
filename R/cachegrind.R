5a3c36dd0fe549f0226a767eb7ef15d1adb6b03d
Luke Tierney <luke-tierney@uiowa.edu>
2018-08-02 13:01:49 -0500

    Added cache grind addon and modified registration for proftools-GUI.

---
+++ R/cachegrind.R
@@ -0,0 +1,22 @@
+cacheGrind <- function(pd) {
+    getCmd <- function(name)
+        tryCatch(system2("which", name, stdout = TRUE),
+                 warning = function(w) NULL)
+    cmd <- getCmd("kcachegrind")
+    if (! is.character(cmd))
+        cmd <- getCMD("qcachegrind")
+    if (! is.character(cmd))
+        stop("neither 'kcachegrind' nor 'qcachecrind' is available")
+    fname <- tempfile()
+    writeCallgrindFile(pd, file = fname)
+    system2(cmd, fname, wait = FALSE)
+    Sys.sleep(1)
+    unlink(fname)
+}
+
+cacheGrindRStudio <- function() {
+    pd <- readRStudioProfileCacheData()
+    if (is.null(pd))
+        stop("no profile data available in the RStudio profile cache")
+    cacheGrind(pd)
+}
